da70bfd6c3d098e98b6e2d4cfc6893113ea5478d
Angelo Pirola <angelo9871@gmail.com>
2023-02-24 22:12:49 +0100

    Creato progetto class library

---
+++ src/GestioneSagre.Data.EFCore/GlobalUsings.cs
@@ -0,0 +1,4 @@
+﻿global using GestioneSagre.Data.EFCore.Core;
+global using GestioneSagre.Data.EFCore.Core.Interfaces;
+global using GestioneSagre.Data.EFCore.Infrastructure.Interfaces;
+global using Microsoft.EntityFrameworkCore;
